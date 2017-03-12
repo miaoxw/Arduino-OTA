931005992294ea94f584ba0ad2a453c6d19280f6
Miau Shiauwe <miau.shiauwe@smail.nju.edu.cn>
2017-03-12 23:53:01 +0800

    Basic modification

--- esp8266_binaryServer/esp8266_binaryServer.ino
+++ esp8266_binaryServer/esp8266_binaryServer.ino
@@ -6,64 +6,57 @@
 #include <ESP8266mDNS.h>
 
 const char* host = "esp8266-webupdate";
-const char* ssid = "........";
-const char* password = "........";
+const char* ssid="........";
+const char* password="........";
 
 ESP8266WebServer server(80);
-const char* serverIndex = "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
+const char* serverIndex="<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
 
-void setup(void){
+void setup(void)
+{
 	Serial.begin(115200);
-	Serial.println();
-	Serial.println("Booting Sketch...");
 	WiFi.mode(WIFI_AP_STA);
-	WiFi.begin(ssid, password);
-	if(WiFi.waitForConnectResult() == WL_CONNECTED){
+	WiFi.begin(ssid,password);
+	if(WiFi.waitForConnectResult()==WL_CONNECTED)
+	{
 		MDNS.begin(host);
-		server.on("/", HTTP_GET, [](){
-			server.sendHeader("Connection", "close");
-			server.sendHeader("Access-Control-Allow-Origin", "*");
-			server.send(200, "text/html", serverIndex);
+		server.on("/",HTTP_GET,[](){
+			server.sendHeader("Connection","close");
+			server.sendHeader("Access-Control-Allow-Origin","*");
+			server.send(200,"text/html",serverIndex);
 		});
-		server.on("/update", HTTP_POST, [](){
-			server.sendHeader("Connection", "close");
-			server.sendHeader("Access-Control-Allow-Origin", "*");
-			server.send(200, "text/plain", (Update.hasError())?"FAIL":"OK");
-			ESP.restart();
+		server.on("/update",HTTP_POST,[](){
+			server.sendHeader("Connection","close");
+			server.sendHeader("Access-Control-Allow-Origin","*");
+			server.send(200,"text/plain",(Update.hasError())?"FAIL":"OK");
 		},[](){
-			HTTPUpload& upload = server.upload();
-			if(upload.status == UPLOAD_FILE_START){
-				Serial.setDebugOutput(true);
+			HTTPUpload& upload=server.upload();
+			if(upload.status==UPLOAD_FILE_START)
+			{
 				WiFiUDP::stopAll();
-				Serial.printf("Update: %s\n", upload.filename.c_str());
-				uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
-				if(!Update.begin(maxSketchSpace)){//start with max available size
-					Update.printError(Serial);
-				}
-			} else if(upload.status == UPLOAD_FILE_WRITE){
-				if(Update.write(upload.buf, upload.currentSize) != upload.currentSize){
-					Update.printError(Serial);
-				}
-			} else if(upload.status == UPLOAD_FILE_END){
-				if(Update.end(true)){ //true to set the size to the current progress
-					Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-				} else {
-					Update.printError(Serial);
-				}
-				Serial.setDebugOutput(false);
+				//TODO
+			}
+			else if(upload.status==UPLOAD_FILE_WRITE)
+			{
+				//TODO
+			}
+			else if(upload.status==UPLOAD_FILE_END)
+			{
+				//TODO
 			}
 			yield();
 		});
 		server.begin();
 		MDNS.addService("http", "tcp", 80);
-
-		Serial.printf("Ready! Open http://%s.local in your browser\n", host);
-	} else {
-		Serial.println("WiFi Failed");
+	}
+	else
+	{
+		ESP.restart();
 	}
 }
 
-void loop(void){
+void loop(void)
+{
 	server.handleClient();
 	delay(1);
-} 
+}
